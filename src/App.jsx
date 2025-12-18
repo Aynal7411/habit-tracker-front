dce06029776bde516586eff97559fee93a4a46c7
aynalhaque <aynalhaque7411@gmail.com>
2025-12-18 12:38:38 +0600

    page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-
+import Login from './pages/Login'
+import Register from './pages/Register'
 import './App.css'
 
 function App() {
@@ -8,6 +9,8 @@ function App() {
   return (
     <>
        <div>
+        <Login />
+        <Register />
 <h1 class="text-3xl font-bold underline">Welcome To Habit tracker  app </h1>
        </div>
        
